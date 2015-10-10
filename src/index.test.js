7e34254bc3c5e6c29c8906a32b9e3395b71be7b7
starandtina <kandmforever@126.com>
2015-10-10 17:35:40 +0800

    Add commitizen cz-conventional-changelog

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names-kz', function () {
+  it('should work!', function () {
+    expect(true).to.be.true;
+  });
+});
